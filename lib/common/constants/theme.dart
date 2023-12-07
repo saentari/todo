6618781cdbfc1015aaac0c1fc9586c4b7c7b73ad
saentari <saentari@protonmail.com>
2023-12-07 09:16:14 +0100

    Updated widget theme styles

--- lib/common/constants/theme.dart
+++ lib/common/constants/theme.dart
@@ -5,4 +5,67 @@ final ThemeData theme = ThemeData(
   useMaterial3: true,
   brightness: Brightness.light,
   fontFamily: 'Inter',
+  splashColor: Colors.transparent,
+  highlightColor: Colors.transparent,
+  scaffoldBackgroundColor: const Color(0xFFFFFFFF),
+
+  // Buttons
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: const MaterialStatePropertyAll(Color(0xFF000000)),
+      foregroundColor: const MaterialStatePropertyAll(Color(0xFFFFFFFF)),
+      textStyle: const MaterialStatePropertyAll(TextStyle(fontSize: 18)),
+      shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))),
+      minimumSize: const MaterialStatePropertyAll(Size.fromHeight(54)),
+    ),
+  ),
+
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: Colors.black,
+    elevation: 1,
+    shape: CircleBorder(),
+  ),
+
+  // Dialogs
+  dialogTheme: DialogTheme(
+    backgroundColor: const Color(0xFFFFFFFF),
+    surfaceTintColor: const Color(0xFFFFFFFF),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+  ),
+
+  // Inputs
+  inputDecorationTheme: InputDecorationTheme(
+    fillColor: Colors.white,
+    contentPadding: const EdgeInsets.all(16),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      borderSide: const BorderSide(color: Color(0xFFD7D7D7)),
+    ),
+    disabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      borderSide: const BorderSide(color: Color(0xFFD7D7D7)),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      borderSide: const BorderSide(color: Color(0xFFD7D7D7)),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      borderSide: const BorderSide(color: Color(0xFFBE5151)),
+    ),
+    focusedErrorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      borderSide: const BorderSide(color: Color(0xFFBE5151)),
+    ),
+  ),
+
+  // App Bar
+  appBarTheme: AppBarTheme(
+    elevation: 0,
+    scrolledUnderElevation: 0,
+    toolbarHeight: 80,
+    shadowColor: Colors.black,
+    backgroundColor: Colors.white,
+    shape: Border(bottom: BorderSide(color: Colors.black.withOpacity(0.1), width: 1)),
+  ),
 );
--- lib/common/widgets/bottom_sheet_widget.dart
+++ lib/common/widgets/bottom_sheet_widget.dart
@@ -11,26 +11,22 @@ class BottomSheetWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-          ),
-          padding: const EdgeInsets.all(24),
-          margin: const EdgeInsets.all(0),
-          width: double.maxFinite,
-          child: Wrap(
-            children: [widget],
-          ),
-        )
-      ],
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+      ),
+      padding: const EdgeInsets.all(24),
+      margin: const EdgeInsets.all(0),
+      width: double.maxFinite,
+      child: Wrap(
+        children: [widget],
+      ),
     );
   }
 
   static void showBottomSheet({required Widget content}) {
     Get.bottomSheet(
+      backgroundColor: const Color(0xFFF8F8F8),
       elevation: 0,
       isScrollControlled: true,
       BottomSheetWidget(widget: content),
